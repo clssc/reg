f3393ba74c32ce361bde7eaca50408f84ba4c96c
Arthur Hsu <artchsu@gmail.com>
2015-01-26 00:10:19 -0800

    Make charge.php to bookkeep

--- client/resources/charge.php
+++ client/resources/charge.php
@@ -13,5 +13,30 @@
       'description' => $familyId . ' ' . $email
   ));
 
-  echo '<span>OK</span><span>' . $familyId . '</span><span>' . strval($dollarAmount) . '</span>'
+  $url = 'https://script.google.com/macros/s/AKfycbzP9pdLR20Ldb1X6a65X3PopwHL-yL4RsxoAxYnM_on_PhV_vQ/exec';
+  $postdata = array('data' => json_encode(array(
+    'id' => $charge['id'],
+    'description' => $charge['description'],
+    'livemode' => $charge['livemode'],
+    'paid' => $charge['paid'],
+    'amount' => $charge['amount'],
+    'currency' => $charge['currency'],
+    'refunded' => $charge['refunded'],
+    'captured' => $charge['captured'],
+    'card' => array(
+      'id' => $charge['card']['id'],
+      'last4' => $charge['card']['last4'],
+      'brand' => $charge['card']['brand'],
+      'name' => $charge['card']['name']
+    )
+  )));
+  $ch = curl_init();
+  curl_setopt($ch, CURLOPT_URL, $url);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+  curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+  curl_setopt($ch, CURLOPT_POST, 1);
+  curl_setopt($ch, CURLOPT_POSTFIELDS, $postdata);
+  $result = curl_exec($ch);
+
+  echo '<span>OK</span><span>' . $familyId . '</span><span>' . strval($dollarAmount) . '</span><span>' . $result . '</span>'
 ?>
