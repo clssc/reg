2df6ed976c9ed5b2a350dab74aa3dd6f92b11ebc
Arthur Hsu <artchsu@gmail.com>
2015-04-22 17:35:15 -0700

    Force redirect on landing page to avoid possible charging failures due to mismatched origin

--- client/resources/detect.js
+++ client/resources/detect.js
@@ -1,6 +1,13 @@
 $(function() {
   $('#browser_warning').hide();
 
+  // Force browser redirection if the origin is wrong.
+  var URL = 'https://www.westsidechineseschool.com';
+  if (window.location.origin != URL) {
+    window.location.href = URL + '/reg';
+    return;
+  }
+
   var isChrome = navigator.userAgent.indexOf('Chrome') > -1;
   var isIE = navigator.userAgent.indexOf('MSIE') > -1;
   var isFirefox = navigator.userAgent.indexOf('Firefox') > -1;
